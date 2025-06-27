2a5fc485bc7c125bbefcb915bdaf44eed17760ee
Shakleen Ishfar <shakleenishfar@gmail.com>
2025-06-27 12:12:02 -0400

    <remove> Kanata number bindings removed
    Number bindings were annoying to use.

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -11,65 +11,10 @@ map("n", "<leader>wsq", 'ciw""<Esc>P')
 
 local harpoon = require("harpoon")
 
--- basic telescope configuration
-local conf = require("telescope.config").values
-
--- Helper function to create the finder based on the current harpoon list
-local function create_harpoon_finder()
-    local harpoon_items = harpoon:list().items
-    local file_paths = {}
-    for _, item in ipairs(harpoon_items) do
-        table.insert(file_paths, item.value)
-    end
-    return require("telescope.finders").new_table({
-        results = file_paths,
-    })
-end
-
-local function toggle_telescope()
-    require("telescope.pickers")
-        .new({}, {
-            prompt_title = "Harpoon",
-            finder = create_harpoon_finder(), -- Use the helper function here
-            previewer = false,
-            sorter = conf.generic_sorter({}),
-            layout_strategy = "center",
-            layout_config = {
-                preview_cutoff = 1,
-                width = function(_, max_columns, _)
-                    return math.min(max_columns, 80)
-                end,
-                height = function(_, _, max_lines)
-                    return math.min(max_lines, 15)
-                end,
-            },
-            borderchars = {
-                prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
-                results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
-                preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-            },
-            attach_mappings = function(prompt_buffer_number, map)
-                -- The keymap you need
-                map("i", "<C-d>", function()
-                    local state = require("telescope.actions.state")
-                    local selected_entry = state.get_selected_entry()
-                    local current_picker = state.get_current_picker(prompt_buffer_number)
-
-                    -- This is the line you need to remove the entry
-                    harpoon:list():remove(selected_entry)
-                    current_picker:refresh(create_harpoon_finder()) -- Refresh with the updated finder
-                end)
-
-                return true
-            end,
-        })
-        :find()
-end
-
 -- Only one of the following should be active. Either telescope or harpoon.ui
 -- Harpoon list with telescope
 map("n", "<leader>wh", function()
-    toggle_telescope()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
 end, { desc = "Open harpoon window" })
 -- harpoon list with harpoon.ui
 -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
